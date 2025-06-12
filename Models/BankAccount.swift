8ddeb54b52081d9bd116020a33d1105dcb8b5519
User <user@MacBook-Air-User-2.local>
2025-06-13 00:00:21 +0300

    Домашка 1: реализация бизнес-логики

---
+++ Models/BankAccount.swift
@@ -0,0 +1,17 @@
+//
+//  BankAccount.swift
+//  MoneyDetector
+//
+//  Created by User on 10.06.2025.
+//
+
+import Foundation
+struct BankAccount: Codable {
+    let id: Int
+    let userId : Int
+    let name : String
+    let balance : Decimal
+    let currency : String
+    let createdAt : String
+    let updatedAt : String
+}
