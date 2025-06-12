53f98114cceb14eaf802d937c1d40877e883419d
User <user@MacBook-Air-User-2.local>
2025-06-12 16:06:08 +0300

    Initial commit with homework

---
+++ MoneyDetector/MoneyDetectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyDetectorApp.swift
+//  MoneyDetector
+//
+//  Created by User on 10.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct MoneyDetectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UnitTest/TestPars.swift
@@ -0,0 +1,7 @@
+//
+//  ParseJSON.swift
+//  MoneyDetector
+//
+//  Created by User on 11.06.2025.
+//
+
